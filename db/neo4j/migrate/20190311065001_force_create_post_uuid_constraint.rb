05f32d425ff20898361245679a12c88a397a977f
tinhnvp <tinhnvp@nustechnology.com>
2019-03-24 05:18:05 -0400

    push

--- db/neo4j/migrate/20190311065001_force_create_post_uuid_constraint.rb
+++
@@ -1,9 +0,0 @@
-class ForceCreatePostUuidConstraint < Neo4j::Migrations::Base
-  def up
-    add_constraint :Post, :uuid, force: true
-  end
-
-  def down
-    drop_constraint :Post, :uuid
-  end
-end
