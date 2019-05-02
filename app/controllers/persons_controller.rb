cb89a5847334db604292af7a02080a3b0a25dd2b
Do Huynh Nhu <huynhnhudo10@gmail.com>
2019-05-02 11:39:40 +0700

    add follow

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -1,12 +1,13 @@
 class PersonsController < ApplicationController
-  layout "persons"
+  layout "groups"
+  before_action :get_user
 
   def profile
   end
 
   def overview
     # tam thoi de day, chú ý khi xong thì chuyển xống index
-    @posts = current_person.posts.all.order(updated_at: :asc)
+    @posts = @person.posts.all.order(updated_at: :asc)
   end
 
   def index
@@ -55,6 +56,27 @@ class PersonsController < ApplicationController
   end
 
   def feed_setting
+  end
+
+  def follow
+    if params[:type] == 'follows'
+      @person.followers << current_person
+      current_person.followings << @person
+    else
+      @person.followers.delete(current_person)
+      current_person.followings.delete(@person)
+    end
+    respond_to do |format|
+      format.html
+      format.js {render :follow}
+    end
+  end
+
+  private
+
+  def get_user
+    return unless params[:id]
+    @person = Person.find(params[:id])
   end
 
 end
